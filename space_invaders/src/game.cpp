15497c46277a46749968b7853df09066f41e9b49
yao <yaoserrato@outlook.com>
2025-09-14 23:16:05 -0600

    chore(space-invaders): create and move spaceship

---
+++ space_invaders/src/game.cpp
@@ -0,0 +1,30 @@
+#include "game.hpp"
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::Draw()
+{
+    spaceship.Draw();
+}
+
+void Game::Update()
+{
+}
+
+void Game::HandleInput()
+{
+    if(IsKeyDown(KEY_LEFT))
+    {
+        spaceship.MoveLeft();
+    }
+    else if(IsKeyDown(KEY_RIGHT))
+    {
+        spaceship.MoveRight();
+    }
+}
