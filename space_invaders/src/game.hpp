cad2e849285762ccc2cbf8de831ec41a1d93fba7
yao <yaoserrato@outlook.com>
2025-09-16 22:25:22 -0600

    chore(space-invaders): create obstacles

--- space_invaders/src/game.hpp
+++ space_invaders/src/game.hpp
@@ -1,6 +1,7 @@
 #pragma once // This header file will be included only once in a compilation unit
 
 #include "spaceship.hpp"
+#include "obstacle.hpp"
 
 class Game
 {
@@ -13,6 +14,8 @@ class Game
 
     private:
         void DeleteInactiveLasers();
+        void CreateObstacles(unsigned int numberOfObstacles);
 
         Spaceship spaceship;
+        std::vector<Obstacle> obstacles;
 };
---
+++ space_invaders/src/obstacle.cpp
@@ -0,0 +1,45 @@
+#include "obstacle.hpp"
+
+std::vector<std::vector<unsigned int>> Obstacle::grid = 
+{
+    {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1}
+};
+
+Obstacle::Obstacle(Vector2 position)
+{
+    this->position = position;
+
+    for(unsigned int row = 0; row < grid.size(); ++row)
+    {
+        for(unsigned int column = 0; column < grid[0].size(); ++column)
+        {
+            if(1 == grid[row][column])
+            {
+                float pos_x = position.x + column * BLOCK_WIDTH;
+                float pos_y = position.y + row * BLOCK_HEIGHT;
+                Block this_block = Block((Vector2){pos_x, pos_y});
+                blocks.push_back(this_block);
+            }
+        }
+    }
+}
+
+void Obstacle::Draw()
+{
+    for(auto& single_block: blocks)
+    {
+        single_block.Draw();
+    }
+}
