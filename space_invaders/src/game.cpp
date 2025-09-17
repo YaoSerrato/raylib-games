c5c6911e0775c09d3c4ec37a0a0e33ac7ef31ba5
yao <yaoserrato@outlook.com>
2025-09-16 18:04:16 -0600

    chore(space-invaders): delete inactive lasers

--- space_invaders/src/game.cpp
+++ space_invaders/src/game.cpp
@@ -1,4 +1,5 @@
 #include "game.hpp"
+#include <iostream>
 
 Game::Game()
 {
@@ -13,7 +14,7 @@ void Game::Draw()
     // Draw the spaceship
     spaceship.Draw();
 
-    // Draw all active lasers
+    // Draw all active lasers (this is a range based for loop in C++)
     for(auto& single_laser: spaceship.lasers)
     {
         single_laser.Draw();
@@ -27,6 +28,10 @@ void Game::Update()
     {
         single_laser.Update();
     }
+
+    // Removing all inactive lasers from the vector
+    DeleteInactiveLasers();
+    //std::cout << "Number of active lasers: " << spaceship.lasers.size() << std::endl;
 }
 
 void Game::HandleInput()
@@ -42,5 +47,22 @@ void Game::HandleInput()
     else if(IsKeyDown(KEY_SPACE))
     {
         spaceship.FireLaser();
+    }
+}
+
+void Game::DeleteInactiveLasers()
+{
+    // Removing the laser from the vector actually kills the object (we do not need to kill them manually)
+    // Making use of iterators in C++
+    for(auto it = spaceship.lasers.begin(); it != spaceship.lasers.end(); )
+    {
+        if(!(it->active))
+        {
+            it = spaceship.lasers.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
     }
 }
