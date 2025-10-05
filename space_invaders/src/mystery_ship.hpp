5634a3a31ff8687259a7266707767ca0f5c3bebc
yao <yaoserrato@outlook.com>
2025-10-05 12:01:59 -0600

    chore(space-invaders): create and spawn mystery ship

---
+++ space_invaders/src/mystery_ship.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <raylib.h>
+
+#define MYSTERY_SHIP_MIN_SPAWN_INTERVAL    10
+#define MYSTERY_SHIP_MAX_SPAWN_INTERVAL    20
+
+class MysteryShip
+{
+    public:
+        MysteryShip();
+        ~MysteryShip();
+        void Update();
+        void Draw();
+        void Spawn();
+        bool alive;
+
+    private:
+        Vector2 position;
+        Texture2D image;
+        int speed;
+};
